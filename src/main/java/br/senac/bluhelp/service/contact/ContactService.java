956a854eb6af92052d1bf65113d7d2fd053c335a
HenriqueCustodio <98133422+HenriqueCustodio@users.noreply.github.com>
2022-08-11 14:35:27 -0300

    Foi criado a interface e o service de Contato

--- src/main/java/br/senac/bluhelp/service/contact/ContactService.java
+++ src/main/java/br/senac/bluhelp/service/contact/ContactService.java
@@ -1,5 +1,10 @@
 package br.senac.bluhelp.service.contact;
 
 public interface ContactService {
-
+	
+	void addContact(Contact contact);
+	void removeContact(Long id);
+	void editContact(Contact contact);
+	
+	
 }
