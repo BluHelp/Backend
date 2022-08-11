2048e8401acf6319a26fadb35ccf0f2b52d58aae
HenriqueCustodio <98133422+HenriqueCustodio@users.noreply.github.com>
2022-08-11 14:56:47 -0300

    Foi criado o dto de Endereco

---
+++ src/main/java/br/senac/bluhelp/dto/address/AddressDTO.java
@@ -0,0 +1,5 @@
+package src.main.java.br.senac.bluhelp.dto.address;
+
+public class AddressDTO (Long id, String roadType, String street, short number, String neighborhood, String complement, String zipCode){
+
+}
