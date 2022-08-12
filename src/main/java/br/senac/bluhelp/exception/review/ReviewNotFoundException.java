b53d0083fa5a69f65e766753eccfada357ed1fca
Maria Eduarda Cenci <meduarda.cenci@gmail.com>
2022-08-12 17:20:20 -0300

    Criado Service de Review e Exceção
    - Métodos para projeções foram implementados;
    - Foi criada uma exceção.

---
+++ src/main/java/br/senac/bluhelp/exception/review/ReviewNotFoundException.java
@@ -0,0 +1,9 @@
+package br.senac.bluhelp.exception.review;
+
+public class ReviewNotFoundException extends RuntimeException {
+	
+	public ReviewNotFoundException(String message) {
+		super(message);
+	}
+
+}
