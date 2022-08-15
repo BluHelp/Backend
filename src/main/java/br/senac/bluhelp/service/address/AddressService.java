332c8ec8c0d21a87c584847fc454c2577ea13ebf
HenriqueCustodio <henrique.spilere.custodio@gmail.com>
2022-08-15 17:03:45 -0300

    Foi criado o service de Address

--- src/main/java/br/senac/bluhelp/service/address/AddressService.java
+++ src/main/java/br/senac/bluhelp/service/address/AddressService.java
@@ -1,5 +1,14 @@
+
 package br.senac.bluhelp.service.address;
 
+import br.senac.bluhelp.model.address.Address;
+
 public interface AddressService {
+
+	void addAddress(Address address);
+
+	void deleteAddress(Long id);
+
+	void editAddress(Address address);
 
 }
--- src/main/java/br/senac/bluhelp/service/address/AddressServiceImpl.java
+++ src/main/java/br/senac/bluhelp/service/address/AddressServiceImpl.java
@@ -1,5 +1,40 @@
-package br.senac.bluhelp.service.address;
+ package br.senac.bluhelp.service.address;
 
+import org.springframework.stereotype.Service;
+
+import br.senac.bluhelp.mapper.address.AddressMapper;
+import br.senac.bluhelp.model.address.Address;
+import br.senac.bluhelp.repository.address.AddressRepository;
+
+
+@Service
 public class AddressServiceImpl implements AddressService {
+	
+	private final AddressRepository addressRepository;
+	private final AddressMapper addressMapper;
+	
+	public AddressServiceImpl(AddressRepository addressRepository, AddressMapper addressMapper) {
+		this.addressRepository = addressRepository;
+		this.addressMapper = addressMapper;
+	}
+	
+	
+	public void addAddress(Address address) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void deleteAddress(Long id) {
+		// TODO Auto-generated method stub
+		if (!addressRepository.existsById(id)) {
+			
+		}
+	}
+	
+	public void editAddress(Address address) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
