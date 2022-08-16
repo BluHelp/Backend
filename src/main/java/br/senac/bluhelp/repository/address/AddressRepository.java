759fbef9e503248d7d3196a6d70305048e3eac44
Nicolas <nicolasjosefachini@gmail.com>
2022-08-16 16:43:42 -0300

    Foi criado o repositorio de Endereço

--- src/main/java/br/senac/bluhelp/repository/address/AddressRepository.java
+++ src/main/java/br/senac/bluhelp/repository/address/AddressRepository.java
@@ -1,9 +1,22 @@
 package br.senac.bluhelp.repository.address;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import br.senac.bluhelp.model.address.Address;
-
+import br.senac.bluhelp.projection.address.AddressProjection;
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+	
+	boolean existsById(Long id);
+	
+	Optional <AddressProjection> findAddressById(Long id);
+	
+	@Query(value="SELECT a.id AS id, a.street AS street, a.number AS number, a.neighborhood AS neighborhod, a.cep AS cep FROM Address")
+	List<AddressProjection> findAddresses();
 
 }
